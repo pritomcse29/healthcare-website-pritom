cab3eca8df1eeebc36b80fcbbe49681a2330e64b
pritomcse29 <jatisarkar.29@gmail.com>
2021-10-29 09:56:49 +0600

    update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,14 +1,3 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBxc1rJ4gVmMRU4-dIdLP9tn3bvhBmT4h4",
-//     authDomain: "simple-firebase-auths.firebaseapp.com",
-//     projectId: "simple-firebase-auths",
-//     storageBucket: "simple-firebase-auths.appspot.com",
-//     messagingSenderId: "1037927036710",
-//     appId: "1:1037927036710:web:c88326fd0743ffb81f73e7"
-// };
-// export default firebaseConfig;
-
-
 const firebaseConfig = {
     apiKey: "AIzaSyBxc1rJ4gVmMRU4-dIdLP9tn3bvhBmT4h4",
     authDomain: "simple-firebase-auths.firebaseapp.com",
